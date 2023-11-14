73e800110a2b5b3d3cf40ba7248887928f742bea
ElektroKill <elektrokilldev@protonmail.com>
2023-11-14 21:29:42 +0100

    Remove constructor to comply with .NET obsolescence rules

--- ICSharpCode.Decompiler/DecompilerException.cs
+++ ICSharpCode.Decompiler/DecompilerException.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) 2011 AlphaSierraPapa for the SharpDevelop Team
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 // PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
@@ -28,14 +28,9 @@ namespace ICSharpCode.Decompiler
 	public class DecompilerException : Exception, ISerializable
 	{
 		public MethodDef DecompiledMethod { get; set; }
-		
-		public DecompilerException(MethodDef decompiledMethod, Exception innerException) 
-			: base("Error decompiling " + decompiledMethod.FullName + Environment.NewLine, innerException)
-		{
-		}
 
-		// This constructor is needed for serialization.
-		protected DecompilerException(SerializationInfo info, StreamingContext context) : base(info, context)
+		public DecompilerException(MethodDef decompiledMethod, Exception innerException)
+			: base("Error decompiling " + decompiledMethod.FullName + Environment.NewLine, innerException)
 		{
 		}
 	}
